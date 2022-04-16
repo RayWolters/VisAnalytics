2820903cd93de24087f5c14ad53e28f2dc812bc3
20183046 <t.a.w.v.zoggel@student.tue.nl>
2022-04-16 23:53:59 +0200

    changed datasets

--- heatmap.py
+++ heatmap.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 import pandas as pd
 def heatmap(interval):
-    df = pd.read_csv('data/networkplot_data/heatmap/similarities_{}m.csv'.format(str(interval))).set_index('date')
+    df = pd.read_csv('data/networkplot_data/heatmap/smaller_network/similarities_{}m.csv'.format(str(interval))).set_index('date')
 
     fig = px.imshow(df)
     return fig
