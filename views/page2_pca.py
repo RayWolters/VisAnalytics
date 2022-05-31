097bad208bd9a37104457c208f1c5894a41b7aac
Sevda4902 <sevda4902@gmail.com>
2022-05-31 16:02:19 +0200

    change tsne to pca

--- views/page2_pca.py
+++ views/page2_pca.py
@@ -7,7 +7,6 @@ import seaborn as sns
 import plotly.express as px
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import PCA
-from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 
 path_articles = 'data/articles/'
@@ -23,31 +22,31 @@ def sorted_alphanumeric(path):
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
     return sorted(path, key=alphanum_key)
 
-def create_tsne():
+def create_pca():
     """
-    This function will performs the TSNE dimensionality reduction on the articles, historical documents, resumes.
+    This function will performs the PCA dimensionality reduction on the articles.
     """
     ordered_dir_articles = sorted_alphanumeric(os.listdir(path_articles))
-    documents = [open(path_articles + f).read() for f in ordered_dir_articles]
+    documents = [open("data/articles/{}".format(f)).read() for f in os.listdir("data/articles")]
     tfidf = TfidfVectorizer().fit_transform(documents)
-    tsne = TSNE(n_components=2)
-    df = tsne.fit_transform(tfidf.todense())
+    pca = PCA(n_components=2)
+    df = pca.fit_transform(tfidf.todense())
     return df
 
 
-def plot_tsne_kmeans(k, df):
+def plot_pca_kmeans(k, df):
     """
-    This function will take the tsne's as input and convert it to a k-means plot
+    This function will take the PCA's as input and convert it to a k-means plot
     k  -> number of means (clusters)
     df -> dataframe containing tsne values 
     """
     kmeans = KMeans(n_clusters= k)
     label = kmeans.fit_predict(df)
-    dff = pd.DataFrame(df, columns=['tsne1', 'tsne2'])
+    dff = pd.DataFrame(df, columns=['pca1', 'pca2'])
     dff['label'] = [str(i) for i in label]
     dff['label2']= label
     dff['article'] = [i for i in range(845)]
     df = dff.sort_values(by='label2')
-    df = df[['tsne1', 'tsne2', 'label', 'article']]
-    return px.scatter(df, x='tsne1', y='tsne2', color="label", hover_data=['article'])
+    df = df[['pca1', 'pca2', 'label', 'article']]
+    return px.scatter(df, x='pca1', y='pca2', color="label", hover_data=['article'])
     
